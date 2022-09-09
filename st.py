3212d341a823225a266967d67e39239cf89a0e8b
hyunwjang <113203610+hyunwjang@users.noreply.github.com>
2022-09-10 03:42:27 +0900

    Update st.py

--- st.py
+++ st.py
@@ -115,7 +115,8 @@ df8.columns = ['title', 'count']
 df_1 = df8
 # print(df_1)
 wc = df_1.set_index('title').to_dict()['count']
-font = ImageFont.load_default()
+#font = ImageFont.load_default()
+font = ImageFont.load("arial.pil")
 #font = 'C:\Windows\Fonts\HMFMPYUN.ttf'
 wordCloud = WordCloud(font_path=font,
                       width=400, height=400, 
