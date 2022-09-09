c4f8b27b8ce05528e8c6dd5880822c6728373884
hyunwjang <113203610+hyunwjang@users.noreply.github.com>
2022-09-10 03:46:13 +0900

    Update st.py

--- st.py
+++ st.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-form PIL import Image, ImageFont
+from PIL import Image, ImageFont
 
     
 st.title('데이터 분석가를 꿈꾸는 장현우..')
