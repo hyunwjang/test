4fa7cb47e925562f17c1579330096edff9cb2354
hyunwjang <113203610+hyunwjang@users.noreply.github.com>
2022-12-12 13:30:31 +0900

    Update st.py

--- st.py
+++ st.py
@@ -13,7 +13,7 @@ from PIL import ImageFont
    
 st.subheader("뉴스기사 제목 워드클라우드")     
 news_num =  st.number_input('페이지수', 100)
-query  = st.text_input('네이버 검색어', '쿠팡')
+query  = st.text_input('네이버 검색어','')
 # news_num  = 100
 #news_num =  st.number_input(뉴스기사 수, value)
 
